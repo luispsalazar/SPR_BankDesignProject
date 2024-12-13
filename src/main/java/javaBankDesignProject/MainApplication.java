57b1c82256b783ec81fffaadb8fe02d41951d34c
Luis Escobar <luispsalazar@yahoo.com>
2024-12-12 22:49:17 -0500

    Swagger added

--- src/main/java/javaBankDesignProject/MainApplication.java
+++ src/main/java/javaBankDesignProject/MainApplication.java
@@ -3,7 +3,10 @@ package javaBankDesignProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MainApplication {
 
     public static void main(String[] args) {
